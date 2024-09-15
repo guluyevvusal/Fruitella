2dc88baa3acc07212f66f6d4d4da710d52d3f42c
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-09-15 18:07:22 +0400

    Migrations and Generic Repository created. There were some corrections

---
+++ DataAccessLayer/Fruitella.DataAccessLayer/Repository/GenericRespository.cs
@@ -0,0 +1,50 @@
+﻿using Fruitella.DataAccessLayer.Abstract;
+using Fruitella.DataAccessLayer.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitella.DataAccessLayer.Repository
+{
+    public class GenericRespository<T> : IGenericDAL<T> where T : class
+    {
+
+        private readonly FruitellaContext _context;
+
+        public GenericRespository(FruitellaContext context)
+        {
+            _context = context;
+        }
+
+        public void Delete(int id)
+        {
+            var value = _context.Set<T>().Find(id);
+            _context.Set<T>().Remove(value);
+            _context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+           return _context.Set<T>().ToList();
+        }
+
+        public T GetByİd(int id)
+        {
+            return _context.Set<T>().Find(id);
+        }
+
+        public void Insert(T entity)
+        {
+           _context.Set<T>().Add(entity);
+            _context.SaveChanges();
+        }
+
+        public void Update(T entity)
+        {
+            _context.Set<T>().Update(entity);
+            _context.SaveChanges();
+        }
+    }
+}
