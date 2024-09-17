9e6cb1549a8a7c6abf34419c26c95cf92ec9b209
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-09-17 11:04:24 +0400

    BusinessLayer.Interfaces created

---
+++ BusinessLayer/Fruitella.BusinessLayer/Abstract/IFeatureService.cs
@@ -0,0 +1,13 @@
+﻿using Fruitella.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitella.BusinessLayer.Abstract
+{
+    public interface IFeatureService : IGenericService<Feature>
+    {
+    }
+}
---
+++ BusinessLayer/Fruitella.BusinessLayer/Abstract/IGenericService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitella.BusinessLayer.Abstract
+{
+    public interface IGenericService<T> where T : class
+
+    {
+        void TInsert(T entity);
+        void TUpdate(T entity);
+        void TDelete(int id);
+        List<T> TGetAll();
+        T TGetByİd(int id);
+    }
+}
