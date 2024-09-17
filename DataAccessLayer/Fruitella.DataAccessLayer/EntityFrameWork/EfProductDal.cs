b0f903f0eb20a97c88851d3dd5df65b4fd1c2786
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-09-17 10:53:51 +0400

    Interfaces and EfCoreDAl created

---
+++ DataAccessLayer/Fruitella.DataAccessLayer/EntityFrameWork/EfProductDal.cs
@@ -0,0 +1,30 @@
+﻿using Fruitella.DataAccessLayer.Abstract;
+using Fruitella.DataAccessLayer.Context;
+using Fruitella.DataAccessLayer.Repository;
+using Fruitella.EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitella.DataAccessLayer.EntityFrameWork
+{
+    public class EfProductDal : GenericRespository<Product>, IProductDAL
+    {
+        private readonly FruitellaContext _context;
+
+        public EfProductDal(FruitellaContext context) : base(context)
+        {
+            _context = context;
+        }
+
+
+        public List<Product> ProductListWithCategory()
+        {
+            var values = _context.Products.Include(x=>x.Category).ToList();
+            return values;
+        }
+    }
+}
---
+++ DataAccessLayer/Fruitella.DataAccessLayer/EntityFrameWork/EfSliderDal.cs
@@ -0,0 +1,19 @@
+﻿using Fruitella.DataAccessLayer.Abstract;
+using Fruitella.DataAccessLayer.Context;
+using Fruitella.DataAccessLayer.Repository;
+using Fruitella.EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fruitella.DataAccessLayer.EntityFrameWork
+{
+    public class EfSliderDal : GenericRespository<Slider>, ISliderDAL
+    {
+        public EfSliderDal(FruitellaContext context) : base(context)
+        {
+        }
+    }
+}
