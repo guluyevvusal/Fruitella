46799376cfd29b18a920223f18789b7f911bb4e9
vüsal <vsal@LAPTOP-8PPT5PTN>
2024-09-27 21:15:45 +0400

    ViewComponents were created

---
+++ PresentationLayer/Fruitella.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_HeadLayoutComponentsPartial.cs
@@ -0,0 +1,17 @@
+﻿
+
+
+
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fruitella.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+    public class _HeadLayoutComponentsPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
